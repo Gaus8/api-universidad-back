88cfe95efce1f5eb014e08a762a455de4c98db21
Stiven Garzon <usg210802@outlook.com>
2025-09-22 21:32:22 -0500

    chore: configuracion para el uso de rutas

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 import express from 'express'
-import 'dotenv/config';
-import cors from 'cors';
-
+import cors from 'cors'
+import 'dotenv/config'
 import { dbConnection } from './src/database/dbConnection.js';
+import { routerEstudiante } from './src/routes/estudianteRoutes.js';
 
 const app = express()
-const PORT =  process.env.PORT ?? 8080;
-app.use(express.json());
-app.use(cors);
 
-;
-app.listen(PORT,async () =>{
-await  dbConnection()
+app.use(express.json());
+app.use(cors())
+
+app.use('/api', routerEstudiante)
+
+const PORT =  process.env.PORT ?? 8000;
+
+
+app.listen(PORT, async() =>{
+await  dbConnection();
   console.log(`server listening on port http://localhost:${PORT}`)
 })
 
